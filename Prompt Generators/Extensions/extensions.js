bebc46e0a7050cfedae875db67d00abb952ceb3d
josiahrippelmeyer <josiahrippelmeyer@gmail.com>
2023-04-10 22:27:56 -0500

    Extension Done with new checkbox
    need to update checkboxes on others

---
+++ Prompt Generators/Extensions/extensions.js
@@ -0,0 +1,90 @@
+const form = document.getElementById("prompt-form");
+const grade = document.getElementById("grade");
+const subject = document.getElementById("subject");
+const standard = document.getElementById("standard");
+const number = document.getElementById("number");
+const environment = document.getElementById("environment");
+const resources = document.getElementsByName("resource");
+const errorMsg = document.getElementById("errorMsg");
+const copyPromptButton = document.getElementById("copy-prompt-button");
+
+function typeWriter(txt) {
+  var i = 0;
+  var speed = 25;
+  function type() {
+    if (i < txt.length) {
+      document.getElementById("aiPrompt").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(type, speed);
+    }
+  }
+  type();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // Get the selected resources
+  const selectedResources = [];
+  for (let resource of resources) {
+    if (resource.checked) {
+      selectedResources.push(resource.value);
+    }
+  }
+
+  // Check if all required fields are filled out
+  if (
+    grade.value === "" ||
+    subject.value === "" ||
+    standard.value === "" ||
+    number.value === "" ||
+    environment.value === "" ||
+    selectedResources.length === 0
+  ) {
+    errorMsg.classList.remove("hidden");
+    return;
+  }
+
+  // Generate the prompt
+  const prompt = `Create ${
+    number.value
+  } extension activities for students who have demonstrated mastery of the essential standard "${
+    standard.value
+  }" in a ${grade.value} grade ${subject.value} class. Consider the learning environment: ${
+    environment.value
+  }. The following digital resources are available for student use: ${selectedResources.join(
+    ", "
+  )}. Make sure the activities are engaging, promote higher-order thinking, and challenge the students to apply their knowledge in new ways.`;
+
+  // Enable the copy prompt button and remove error message if necessary
+  copyPromptButton.disabled = false;
+  copyPromptButton.classList.remove("disabled");
+  copyPromptButton.classList.add("enabled");
+  copyPromptButton.classList.add("hover");
+  copyPromptButton.classList.add("active");
+  errorMsg.classList.add("hidden");
+
+  // Display the prompt with the typewriter animation
+  typeWriter(prompt);
+}
+
+function copyPrompt() {
+  // Get the text field
+  var copyText = document.getElementById("aiPrompt");
+
+  // Select the text field
+  var range = document.createRange();
+  range.selectNode(copyText);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+
+  // Copy and alert
+  document.execCommand("copy");
+  alert("Copied the prompt");
+
+  // Open a link to chat.openai.com
+  window.open("http://chat.openai.com", "_blank");
+}
+
+form.addEventListener("submit", handleSubmit);
+copyPromptButton.addEventListener("click", copyPrompt);
