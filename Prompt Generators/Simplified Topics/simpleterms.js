eb22fa7fdf99544e609bd8de142bf83a6579296b
josiahrippelmeyer <josiahrippelmeyer@gmail.com>
2023-04-11 22:06:43 -0500

    completed simplify complex

--- Prompt Generators/Simplified Topics/simpleterms.js
+++ Prompt Generators/Simplified Topics/simpleterms.js
@@ -4,7 +4,6 @@ const topic = document.getElementById("topic");
 const grade = document.getElementById("grade");
 const subject = document.getElementById("subject");
 const points = document.getElementById("points");
-const format = document.getElementById("format");
 const errorMsg = document.getElementById("errorMsg");
 const promptDisplay = document.getElementById("aiPrompt");
 const copyPromptButton = document.getElementById("copy-prompt-button");
@@ -27,7 +26,11 @@ function typeWriter(txt) {
 function handleSubmit(event) {
   event.preventDefault();
 
-  if (topic.value === "" || grade.value === "" || subject.value === "" || format.value === "") {
+  // Get the selected format
+  const format = document.querySelector('input[name="format"]:checked')?.value;
+
+  // Check if all required fields are filled out
+  if (topic.value === "" || grade.value === "" || subject.value === "" || format === "") {
     errorMsg.classList.remove("hidden");
     return;
   }
@@ -39,7 +42,8 @@ function handleSubmit(event) {
   const pointsString =
     pointsArray.length > 0 ? ` In particular, focus on the following points: ${pointsArray.join(", ")}.` : "";
 
-  const prompt = `I have a ${grade.value} ${subject.value} class, and I need to explain the complex topic of ${topic.value} in simple terms for my students.${pointsString} Can you provide a simplified explanation in the format of ${format.value}? As you create the explanation, consider the students' age and background knowledge, and break down the complex ideas into more easily understandable concepts.`;
+  // Generate the prompt
+  const prompt = `I need help simplifying the topic "${topic.value}" for my ${grade.value} grade ${subject.value} class.${pointsString} I want the explanation to be in ${format} format. Please consider my students' age and background knowledge when breaking down this complex idea into more understandable terms.`;
 
   // Enable the copy prompt button and hide error message
   copyPromptButton.disabled = false;
